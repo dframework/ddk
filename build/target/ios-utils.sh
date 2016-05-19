e6fdde24326876ba58e9d62dd92ab984c3ef610a
HyunKyu Choi <chk@chkmac.local>
2016-05-19 19:45:37 +0900

    .

--- build/target/ios-utils.sh
+++ build/target/ios-utils.sh
@@ -50,30 +50,39 @@ ddk_crosscp_ready(){
         VERSION=`xcrun -sdk iphoneos --show-sdk-version`
     fi
 
-    local CFLAGS="${DDK_CROSS_CFLAGS} -pipe -Wno-unused-value"
-    case $CFLAGS in
-        armv7s) CFLAGS="${CFLAGS} -miphoneos-version-min=6" ;;
-        arm64) CFLAGS="${CFLAGS} -miphoneos-version-min=7" ;;
-        x86_64) CFLAGS="${CFLAGS} -miphoneos-version-min=8" ;;
+    local cflags="${DDK_CROSS_CFLAGS} -pipe -Wno-unused-value -arch ${ARCH}"
+    case $cflags in
+        armv7s) cflags="${cflags} -miphoneos-version-min=6" ;;
+        arm64) cflags="${cflags} -miphoneos-version-min=7" ;;
+        x86_64) cflags="${cflags} -miphoneos-version-min=8" ;;
     esac
 
-    CFLAGS="${CFLAGS} -arch ${ARCH} -mcpu=cortex-a8 -marm"
+    case ${ARCH} in
+        armv7) cflags="${cflags} -mcpu=cortex-a8" ;;
+        armv7s) cflags="${cflags} -mcpu=cortex-a8" ;;
+        arm64) cflags="${cflags} -Qunused-arguments" ;;
+        i386) cflags="${cflags} -Qunused-arguments" ;;
+        x86_64) cflags="${cflags} -Qunused-arguments" ;;
+    esac
+
+    cflags="${cflags} -marm"
+
     if [[ $VERSION == 9.* ]]; then
-        CFLAGS="${CFLAGS} -fembed-bitcode"
+        cflags="${cflags} -fembed-bitcode"
     fi
 
     local XCODE_SDK=${PLATFORM}${VERSION}.sdk
     local DEVROOT=${XCODE_PATH}/Platforms/${PLATFORM}.platform/Developer
     local SDKROOT=${DEVROOT}/SDKs/${XCODE_SDK}
 
-    DDK_CROSS_CFLAGS="${CFLAGS} -isysroot ${SDKROOT}"
+    DDK_CROSS_CFLAGS="${cflags} -isysroot ${SDKROOT}"
     DDK_CROSS_LDFLAGS="${DDK_CROSS_LDFLAGS} -arch ${ARCH} --sysroot ${SDKROOT}"
     DDK_CROSS_HOME="${XCODE_PATH}/Toolchains/XcodeDefault.xctoolchain/usr/bin"
     DDK_CROSS_PREFIX=""
     DDK_ENV_INCLUDES=""
     DDK_CC="clang"
     DDK_CXX="clang++"
-
+    DDK_APPLE_SDK_VERSION=$VERSION
 }
 
 # -------------------------------------------------------
